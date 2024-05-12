5ac6541fdaf162fd3b7dfd607850179433d69563
ylovecha <tianelove0819@163.com>
2024-05-12 17:42:09 +0800

    加上了样式但是很小很迷茫

---
+++ src/components/try-this.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+<script>
+import Swiper from "swiper";
+import "swiper/css/swiper.css";
+export default {
+  mounted() {
+    new Swiper(".swiper-container", {
+      pagination: { el: ".swiper-pagination" },
+    });
+  },
+};
+</script>
+<style scoped>
+.swiper-container {
+  width: 100%;
+  height: 300px;
+}
+.swiper-slide {
+  text-align: center;
+  font-size: 30px;
+  background: #fff;
+}
+</style>
